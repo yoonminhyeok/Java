c6dfe246a7f09c2b8ed974714e46732be7bcddea
홍길동 <nnymh0729@naver.com>
2024-08-28 00:06:06 +0900

    git commit

---
+++ src/HelloJava2.java
@@ -0,0 +1,9 @@
+public class HelloJava2 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello Java");
+        System.out.println("Hello Java2");
+        System.out.println("Hello Java3");
+
+    }
+}
