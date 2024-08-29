5ec73d49413f00d55d6b31c311b116d1d2ea53a5
홍길동 <nnymh0729@naver.com>
2024-08-29 21:02:13 +0900

    git commit

---
+++ src/variable/Var8.java
@@ -0,0 +1,15 @@
+package variable;
+
+public class Var8 {
+    public static void main(String[] args) {
+
+        byte b=127; // -128~127
+        short s=32767; // -32768~32767
+        int i=2147483647; // -2147483648~2147483647
+        long l=9223372036854775807L;
+
+        float f=10.0f;
+        double d =20.0;
+
+    }
+}
---
+++ src/variable/VarEx1.java
@@ -0,0 +1,15 @@
+package variable;
+
+import java.sql.SQLOutput;
+
+public class VarEx1 {
+    public static void main(String[] args) {
+
+        int num1= 4;
+        int num2= 3;
+
+        System.out.println(num1+num2);
+        System.out.println(num1-num2);
+        System.out.println(num1*num2);
+    }
+}
