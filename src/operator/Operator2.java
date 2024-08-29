a7c9bcad7e5bb00dbf0fbd8fb139ac644e8b6eaf
홍길동 <nnymh0729@naver.com>
2024-08-29 21:52:58 +0900

    git commit

---
+++ src/operator/Operator2.java
@@ -0,0 +1,26 @@
+package operator;
+
+public class Operator2 {
+    public static void main(String[] args) {
+
+        String result1= "Hello" + "World";
+
+        System.out.println(result1);
+
+        String s1="String1";
+        String s2="String2";
+        String result2= s1+s2;
+
+        System.out.println(result2);
+
+        String result3= "a + b= "+10;
+
+        System.out.println(result3);
+
+        int num=20;
+        String result4= "a + b= "+num;
+
+        System.out.println(result4);
+
+    }
+}
