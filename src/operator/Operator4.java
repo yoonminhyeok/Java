1b96055c8d0de9df4caf2d60ca4945e36aca6ea9
홍길동 <nnymh0729@naver.com>
2024-08-29 22:21:55 +0900

    git commit

---
+++ src/operator/Operator4.java
@@ -0,0 +1,11 @@
+package operator;
+
+public class Operator4 {
+    public static void main(String[] args) {
+        int sum3= 3*3 + 4*4;
+        int sum4= (3*3)+(4*4);
+
+        System.out.println("sum3= " + sum3);
+        System.out.println("sum4= " + sum4);
+    }
+}
