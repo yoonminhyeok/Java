c1759f492e533bdcf7e784edbd7c850d96ec1493
홍길동 <nnymh0729@naver.com>
2024-09-02 23:30:39 +0900

    git commit

---
+++ src/cond/If2.java
@@ -0,0 +1,16 @@
+package cond;
+
+public class If2 {
+    public static void main(String[] args) {
+        int age=16;
+
+        if(age>=18)
+        {
+            System.out.println("성인입니다");
+        }
+        else
+        {
+            System.out.println("미성년자 입니다");
+        }
+    }
+}
