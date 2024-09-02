57c5a4de2b1ccf2bd5fbc42765c310725b0236ec
홍길동 <nnymh0729@naver.com>
2024-09-02 23:21:20 +0900

    git commit

---
+++ src/operator/OperationEx2.java
@@ -0,0 +1,17 @@
+package operator;
+
+public class OperationEx2 {
+
+    public static void main(String[] args) {
+
+        double var1=1.5;
+        double var2=2.5;
+        double var3=3.5;
+
+        double varSum=var1+var2+var3;
+        double avgSum=varSum/3;
+
+        System.out.println(varSum);
+        System.out.println(avgSum);
+    }
+}
---
+++ src/operator/OperationEx3.java
@@ -0,0 +1,18 @@
+package operator;
+
+public class OperationEx3 {
+
+    public static void main(String[] args) {
+
+        int score=80;
+
+        if(score >=80 && score<=100)
+        {
+            System.out.println(true);
+        }
+        else
+        {
+            System.out.println(false);
+        }
+    }
+}
