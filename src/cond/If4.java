ed9c323a0df130e4490b1705609368916e57b888
홍길동 <nnymh0729@naver.com>
2024-09-02 23:37:23 +0900

    git commit

--- src/cond/If4.java
+++ src/cond/If4.java
@@ -1,28 +1,28 @@
 package cond;
 
-public class If3 {
+public class If4 {
     public static void main(String[] args) {
 
-        int age = 10;
+        int age = 20;
 
 
         if(age<=7)
         {
             System.out.println("미취학");
         }
-        if(age>=8 && age<=13)
+        else if(age>=8 && age<=13)
         {
             System.out.println("초등학생");
         }
-        if(age>=14 && age<=16)
+        else if(age>=14 && age<=16)
         {
             System.out.println("중학생");
         }
-        if(age>=17 && age<=19)
+        else if(age>=17 && age<=19)
         {
             System.out.println("고등학생");
         }
-        if(age>=20)
+        else
         {
             System.out.println("성인");
         }
