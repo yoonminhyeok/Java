5988a369de95490b2f9455f5a8d6c3bf849673dc
홍길동 <nnymh0729@naver.com>
2024-09-12 22:01:18 +0900

    git commit

---
+++ src/loop/Nested1.java
@@ -0,0 +1,17 @@
+package loop;
+
+public class Nested1 {
+    public static void main(String[] args) {
+
+        for(int i=0; i<2; i++)
+        {
+            System.out.println("외부 for문 시작 i: "+i);
+            for(int j=0; j<3; j++)
+            {
+                System.out.println("내부 for문 "+i+"-"+j);
+            }
+            System.out.println("외부 for문 종료 i: "+i);
+            System.out.println();
+        }
+    }
+}
