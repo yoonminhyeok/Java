c282fb99762d10f3db3cb617b1b84d71c7560e33
홍길동 <nnymh0729@naver.com>
2024-09-12 22:09:08 +0900

    git commit

---
+++ src/ex/WhileEx2.java
@@ -0,0 +1,18 @@
+package ex;
+
+public class WhileEx2 {
+
+    public static void main(String[] args) {
+
+        int i=2;
+
+        while(i<=10)
+        {
+            if(i%2==0)
+            {
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
+}
