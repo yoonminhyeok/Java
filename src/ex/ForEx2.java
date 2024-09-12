6bed284428dfcfdd88310f2bb21668b3f5d84b69
홍길동 <nnymh0729@naver.com>
2024-09-12 22:07:53 +0900

    git commit

---
+++ src/ex/ForEx2.java
@@ -0,0 +1,11 @@
+package ex;
+
+public class ForEx2 {
+    public static void main(String[] args) {
+
+        for(int i=2; i<=10; i+=2)
+        {
+            System.out.println(i);
+        }
+    }
+}
