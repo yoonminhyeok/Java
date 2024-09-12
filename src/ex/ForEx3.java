1ac8ec37fe52717f4637504da352d5d544253e57
홍길동 <nnymh0729@naver.com>
2024-09-12 22:11:49 +0900

    git commit

---
+++ src/ex/ForEx3.java
@@ -0,0 +1,17 @@
+package ex;
+
+public class ForEx3 {
+
+    public static void main(String[] args) {
+
+        int sum=0;
+        int max=3;
+        int i=1;
+        for(i=1; i<=max; i++){
+
+            sum+=i;
+
+        }
+        System.out.println(sum);
+    }
+}
