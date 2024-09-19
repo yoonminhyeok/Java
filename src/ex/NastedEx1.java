674f002fc0e0501a8d9c6c199180f79ca6927a25
홍길동 <nnymh0729@naver.com>
2024-09-19 23:24:44 +0900

    git commit

---
+++ src/ex/NastedEx1.java
@@ -0,0 +1,15 @@
+package ex;
+
+public class NastedEx1 {
+    public static void main(String[] args) {
+
+        for(int i=1; i<=9; i++)
+        {
+            for(int j=1; j<=9; j++)
+            {
+                System.out.println(i+"*"+j+"="+i*j);
+            }
+            System.out.println();
+        }
+    }
+}
