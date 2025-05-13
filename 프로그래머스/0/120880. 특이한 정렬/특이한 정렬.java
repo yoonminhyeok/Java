f921510012e9e545d680e5878989dba4b4531655
yoonminhyeok <116967490+yoonminhyeok@users.noreply.github.com>
2025-05-13 16:11:34 +0900

    [level 0] Title: 특이한 정렬, Time: 0.49 ms, Memory: 83.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,29 @@
+class Solution {  //특이한 정렬
+    public int[] solution(int[] numlist, int n) {
+        int[] answer = new int[numlist.length];
+        int minIndex=0;
+        int tmp;
+        for(int i=0; i<answer.length; i++){
+            minIndex=i;
+            for(int j=i+1; j<answer.length; j++){
+                if(Math.abs(numlist[j]-n) <Math.abs(numlist[minIndex]-n)){
+                    minIndex=j;
+                }   
+                else if(Math.abs(numlist[j]-n) ==Math.abs(numlist[minIndex]-n)) {
+                    if(numlist[j]>numlist[minIndex])
+                        minIndex=j;
+                    
+                }  
+                
+            }
+            answer[i]=numlist[minIndex];
+            tmp=numlist[i];
+            numlist[i]=numlist[minIndex];
+            numlist[minIndex]=tmp;
+            
+        }
+        
+        
+        return answer;
+    }
+}
