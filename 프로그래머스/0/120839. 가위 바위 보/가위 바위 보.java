d599b5aebbddb85ae7026d44aa3ecb80fcb57f3d
yoonminhyeok <116967490+yoonminhyeok@users.noreply.github.com>
2025-05-19 15:49:23 +0900

    [level 0] Title: 가위 바위 보, Time: 6.07 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120839. 가위 바위 보/가위 바위 보.java
@@ -0,0 +1,18 @@
+class Solution { //가위바위 보
+    public String solution(String rsp) {
+        String answer = "";
+        
+        for(int i=0; i<rsp.length(); i++){
+            if(rsp.charAt(i)=='2'){
+               answer+="0"; 
+            }
+            else if(rsp.charAt(i)=='0'){
+                answer+="5";
+            }
+            else{
+                answer+="2";
+            }
+        }
+        return answer;
+    }
+}
