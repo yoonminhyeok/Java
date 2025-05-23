ac450013a92d956ee514a88dfb3f05a5dcf944f3
yoonminhyeok <116967490+yoonminhyeok@users.noreply.github.com>
2025-05-23 09:29:55 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.02 ms, Memory: 72 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.java
@@ -0,0 +1,11 @@
+class Solution {  //머쓱이보다 키 큰 사람
+    public int solution(int[] array, int height) {
+        int answer = 0;
+        
+        for(int i=0; i<array.length; i++){
+            if(array[i]>height)
+                answer++;
+        }
+        return answer;
+    }
+}
